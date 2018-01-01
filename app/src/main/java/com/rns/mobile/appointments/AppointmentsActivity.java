da5dd912463c78245bd06abf7263991a599bd06e
unknown <rohit1110>
2018-01-01 17:00:24 +0530

    commit changes

--- app/src/main/java/com/rns/mobile/appointments/AppointmentsActivity.java
+++ app/src/main/java/com/rns/mobile/appointments/AppointmentsActivity.java
@@ -35,7 +35,7 @@ public class AppointmentsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_appointments);
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
